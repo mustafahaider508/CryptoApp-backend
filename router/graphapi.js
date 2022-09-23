5308458a1ba534cd5687f580701923b785801697
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2022-09-22 22:14:01 -0700

    updates

--- router/graphapi.js
+++ router/graphapi.js
@@ -3,14 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get("/about" , (req,res) => {
-    const data = axios.get(`https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=365`).then((res) => {
-         console.log(res.data.prices);
-       }).catch((err) => {
-        console.log(err);
-    });  
+router.get("/about" ,async (req,res) => {
+    const data = await axios.get('https://httpbin.org/get', { params: { answer: 42 } });
+   console.log(data.data.args);
     
-       res.send(data);
+    
     })
 
 module.exports = router;
